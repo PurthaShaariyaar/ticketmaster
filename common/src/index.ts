cf97f613ac8271e1e2503580d4462420658e462c
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2024-11-08 14:26:52 -0500

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -6,3 +6,4 @@ export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
 
 export * from './middlewares/error-handler';
+export * from './middlewares/validate-request';
