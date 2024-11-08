37918614c1716080f6c2c76a80379fe692e1b7d9
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2024-11-08 14:56:12 -0500

    Updates

--- common/src/errors/custom-error.ts
+++ common/src/errors/custom-error.ts
@@ -3,7 +3,7 @@ export abstract class CustomError extends Error {
   abstract statusCode: number;
 
   constructor(message: string) {
-    super(message)
+    super(message);
 
     Object.setPrototypeOf(this, CustomError.prototype);
   }
