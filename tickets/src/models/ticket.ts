6cf239fcf975f288584a004aeca29793be841839
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2024-11-20 16:59:25 -0500

    created ticket model, installed shared service into tickets service and updated app.ts and index.ts to connect all updated features and error handlers

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,68 @@
+/**
+ * Import dependencies mongoose, updateIfCurrentPlugin
+ * Create interfaces: Attrs, Doc, Model
+ * Assign const schema = new mongoose.Schema -> toJSON
+ * Update the schema versionKey to version
+ * Plugin the updateIfCurrentPlugin to the schema
+ * Call build -> return a new Ticket Doc
+ * Assign a const Ticket as a mongoose model
+ * Export Ticket
+ */
+
+import mongoose from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+  version: number;
+  orderId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  userId: {
+    type: String,
+    required: true
+  },
+  orderId: {
+    type: String
+  }
+}, {
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+
+    }
+  }
+});
+
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+}
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
+
+export { Ticket }
