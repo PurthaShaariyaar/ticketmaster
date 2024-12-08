676cf2f3f3635d4029ecc74f44016545ab916b24
PurthaShaariyaar <purtha.shaariyaar@ontariotechu.net>
2024-12-08 17:04:50 -0500

    created ci/cd workflow for orders service to run all tests and completed new order feat

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, currentUser, NotFoundError } from '@psticketmaster/common';
+import { errorHandler, NotFoundError, currentUser } from '@psticketmaster/common';
+
+import { newOrderRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -10,11 +12,13 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV != 'test'
+    secure: process.env.NODE_ENV !== 'test'
   })
 );
 
 app.use(currentUser);
+
+app.use(newOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
